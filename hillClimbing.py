ce62f88bbfa2645b4f80f38d35fe4ef43c881312
oden41 <iamoden41@gmail.com>
2016-07-31 13:17:41 +0900

    ログ出力をコメント

--- hillClimbing.py
+++ hillClimbing.py
@@ -1,18 +1,21 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
 import gym
 import numpy
-
-M = 5
-T = 200
-alpha = 0.1
+# import csv
 
 
-def doOneIteration(env):
+def doOneIteration(episode, env):
     theta = 2 * numpy.random.rand(4) - 1
     r_best = 0
     g = 0
 
+    # f = open('data{0}.csv'.format(episode), 'ab')
+    # csvWriter = csv.writer(f)
+
     while g < 2000 and r_best < 200 * M:
-        theta_new = theta.__add__(alpha * (2 * numpy.random.rand(4) - 1))
+        # dataList = [g]
+        theta_new = theta + alpha * (2 * numpy.random.rand(4) - 1)
         r_total = 0
         for m in range(M):
             observation = env.reset()
@@ -30,9 +33,29 @@ def doOneIteration(env):
         if r_total > r_best:
             r_best = r_total
             theta = theta_new
+        # dataList.append(r_best)
+        # csvWriter.writerow(dataList)
         g += 1
     print("g:{0},   r_best:{1},   theta:{2}".format(g, r_best, theta))
+    # f.close()
+    if g >= 2000:
+        return False
+    else:
+        return True
 
 
-env = gym.make('CartPole-v0')
-doOneIteration(env)
+if __name__ == "__main__":
+    M = 5
+    T = 200
+    alpha = 0.1
+    success = 0
+    fail = 0
+
+    for i in range(100):
+        env = gym.make('CartPole-v0')
+        result = doOneIteration(i, env)
+        if result:
+            success += 1
+        else:
+            fail += 1
+    print("success:{0},fail={1}".format(success, fail))
